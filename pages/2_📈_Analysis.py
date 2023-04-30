6d14a9110eaccaa38906e5939a2c9626420b1cc3
Ridhiwan <ridhiwanmseya@gmail.com>
2023-04-30 23:00:10 +0300

    added icons

--- pages/2_📈_Analysis.py
+++ pages/2_📈_Analysis.py
@@ -146,11 +146,12 @@ def analysis():
         st.markdown(hide_st_style,unsafe_allow_html=True)
 
     else:
-        with open(r'C:\Users\Zakia\Documents\GitHub\Dashboard\pages\DashyB_logo.gif', "rb") as gif_file:
+        buffer, col, buffer2 = st.columns([1,3,1])
+        with open(r'C:\Users\Zakia\Documents\GitHub\Dashboard\pages\DashyB_logo_cropped.gif', "rb") as gif_file:
             gif_url = base64.b64encode(gif_file.read()).decode("utf-8")
-        st.markdown(f'<img src="data:image/gif;base64,{gif_url}" alt="cat gif">', unsafe_allow_html=True,)
-        st.markdown("""<h1><strong>Welcome to DashyB!</strong></h1>""", unsafe_allow_html=True)
-        st.write("Please upload a CSV or Excel file to analyse.")
+        col.markdown(f'<img src="data:image/gif;base64,{gif_url}" alt="cat gif">', unsafe_allow_html=True,)
+        col.markdown("""<h1><strong>Welcome to DashyB!</strong></h1>""", unsafe_allow_html=True)
+        col.write("Please upload a CSV or Excel file to analyse.")
         st.markdown(hide_st_style,unsafe_allow_html=True)
 
 #---- CREATE PAGE ----
