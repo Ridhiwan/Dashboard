4d888579ef0dc1569d7e460840c31fe0079791c4
Ridhiwan <ridhiwanmseya@gmail.com>
2023-05-03 13:37:28 +0300

    made a logout view

--- pages/Logout.py
+++ pages/Logout.py
@@ -1,4 +1,24 @@
-from SignUp_or_Login import logout
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+#---- HIDE STREAMLIT STYLE ----
+hide_st_style = """ 
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+
+def logout():
+    if "login" not in st.session_state:
+        st.session_state["login"] = False
+        switch_page("SignUp or Login")
+    else:
+        st.session_state["login"] = False
+        switch_page("SignUp or Login")
+
+    st.markdown(hide_st_style,unsafe_allow_html=True)
 
 if __name__ == "__main__":
     logout()
